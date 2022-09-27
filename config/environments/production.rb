2ba9055df3522d96b9818a7feaf17b32905d0c75
Abner del Cid <abner_jonathan@hotmail.com>
2022-09-26 19:19:06 -0600

    Setting cable configs and production database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://shielded-river-42374.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://shielded-river-42374.herokuapp.com", /http:\/\/shielded-river-42374.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
